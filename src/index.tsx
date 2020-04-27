292a0da07b35555ef5b291f9ecbf1986c63b5f1b
Pedro Henrique <pedro00dk@gmail.com>
2020-04-26 22:37:21 -0300

    Ad form fetching from the url hash

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import '@babel/polyfill' // regeneratorRuntime
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Form } from './Form'
 
-ReactDom.render(<div />, document.getElementById('root'))
+const init = async () => {
+    const formAddress = window.location.hash.substring(1)
+    console.log('form address:', formAddress)
+    const response = await fetch(formAddress).catch(() => console.log('failed to fetch form'))
+    const form = response && (await response.json().catch(() => console.log('failed to parse form')))
+    console.log('form content:', form)
+    ReactDom.render(<Form form={form} />, document.getElementById('root'))
+}
+
+init()
