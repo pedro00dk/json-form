2f1e5fd9e8b4b9b5c597c6558a5f0c27605c3e30
Pedro Henrique <pedro00dk@gmail.com>
2020-04-28 16:35:06 -0300

    Aff first functions to test cloud firestore and cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
-const functions = require('firebase-functions');
+const firebase = require('firebase-admin')
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+firebase.initializeApp()
+const db = firebase.firestore()
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send('Hello from Firebase!')
+})
+
+exports.listSubmissions = functions.https.onRequest(async (request, response) => {
+    const collection = await db.collection('submissions').get()
+    const documents = {}
+    collection.forEach(document => (documents[document.id] = document.data()))
+    response.send(JSON.stringify(documents, undefined, 4))
+})
