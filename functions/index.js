0759c3eb77e2a8bd267d9723e6a3d4378422af6d
Pedro Henrique <pedro00dk@gmail.com>
2020-05-02 15:28:27 -0300

    Update cloud functions to match schema

--- functions/index.js
+++ functions/index.js
@@ -39,10 +39,10 @@ const validateSubmission = submission => {
     if (typeof submission !== 'object') throw new Error('submission is not an object')
     if (typeof submission.answers !== 'object') throw new Error('submission.answers is not an object')
     if (typeof submission.times !== 'object') throw new Error('submission.times is not an object')
-    if (typeof submission.sessionOrder !== 'object' || !(submission.sessionOrder instanceof Array))
-        throw new Error('submission.sessionOrder is not an array')
-    if (submission.uniqueAnswer != undefined && typeof submission.uniqueAnswer !== 'string')
-        throw new Error('submission.uniqueAnswer must be undefined or string')
+    if (typeof submission.order !== 'object' || !(submission.order instanceof Array))
+        throw new Error('submission.order is not an array')
+    if (submission.unique != undefined && typeof submission.unique !== 'string')
+        throw new Error('submission.unique must be undefined or string')
 
     const validAnswers = Object.values(submission.answers).reduce(
         (acc, answer) => acc && (answer == undefined || typeof answer === 'string'),
@@ -53,30 +53,23 @@ const validateSubmission = submission => {
         (acc, elapsedTime) => acc && typeof elapsedTime === 'number',
         true
     )
-    if (!validTimes) throw new Error('Some submission time is not number')
-    const validSessionOrder = Object.values(submission.sessionOrder).reduce(
-        (acc, order) => acc && typeof order === 'number',
-        true
-    )
-    if (!validSessionOrder) throw new Error('Some submission session order is not number')
+    if (!validTimes) throw new Error('Some submission time is not a number')
+    const validOrder = Object.values(submission.order).reduce((acc, order) => acc && typeof order === 'number', true)
+    if (!validOrder) throw new Error('Some submission session order is not a number')
 }
 
 const checkUniqueSubmission = async submission => {
-    const uniqueAnswer = submission.uniqueAnswer
-    if (uniqueAnswer == undefined) return true
-    const uniqueAnswerValue = submission.answers[uniqueAnswer]
-        ? submission.answers[uniqueAnswer].trim().toLowerCase()
-        : undefined
+    const unique = submission.unique
+    if (unique == undefined) return true
+    const uniqueValue = submission.answers[unique] ? submission.answers[unique].trim().toLowerCase() : undefined
 
     const collection = await db.collection('submissions').get()
     let isUnique = true
     collection.forEach(document => {
         if (!isUnique) return
         const data = document.data()
-        const documentUniqueAnswerValue = data.answers[uniqueAnswer]
-            ? data.answers[uniqueAnswer].trim().toLowerCase()
-            : undefined
-        if (uniqueAnswerValue === documentUniqueAnswerValue) isUnique = isUnique && false
+        const documentUniqueValue = data.answers[unique] ? data.answers[unique].trim().toLowerCase() : undefined
+        if (uniqueValue === documentUniqueValue) isUnique = isUnique && false
     })
     return isUnique
 }
@@ -102,13 +95,12 @@ exports.acceptSubmission = functions.https.onRequest((request, response) => {
     })
 })
 
-const validateSessionOrder = sessionOrders => {
-    if (!(sessionOrders instanceof Array)) throw new Error('Session order is not an array')
-    for (let sessionOrder of sessionOrders)
-        if (!(sessionOrder instanceof Array)) throw new Error('Session order contains non array order')
+const validateOrders = orders => {
+    if (!(orders instanceof Array)) throw new Error('orders is not an array')
+    for (let order of orders) if (!(order instanceof Array)) throw new Error('orders contains non array order')
 }
 
-const getLessFrequentSessionOrder = async sessionOrders => {
+const getLessFrequentOrder = async orders => {
     const orderEquals = (orderA, orderB) => {
         if (orderA.length !== orderB.length) return false
         for (let i = 0; i < orderA.length; i++) if (orderA[i] !== orderB[i]) return false
@@ -116,28 +108,27 @@ const getLessFrequentSessionOrder = async sessionOrders => {
     }
 
     const collection = await db.collection('submissions').get()
-    const sessionOrderFrequencies = sessionOrders.map(sessionOrder => 0)
+    const frequencies = orders.map(order => 0)
     collection.forEach(document => {
         const data = document.data()
-        const documentSessionOrder = data.sessionOrder
-        for (let i = 0; i < sessionOrders.length; i++) {
-            const sessionOrder = sessionOrders[i]
-            if (orderEquals(sessionOrder, documentSessionOrder)) {
-                sessionOrderFrequencies[i]++
+        const documentOrder = data.order
+        for (let i = 0; i < orders.length; i++) {
+            if (orderEquals(orders[i], documentOrder)) {
+                frequencies[i]++
                 break
             }
         }
     })
 
     let lessFrequent = 0
-    for (let i = 0; i < sessionOrderFrequencies.length; i++)
+    for (let i = 0; i < frequencies.length; i++)
         if (
-            sessionOrderFrequencies[i] < sessionOrderFrequencies[lessFrequent] ||
-            (sessionOrderFrequencies[i] === sessionOrderFrequencies[lessFrequent] && Math.random() < 0.5)
+            frequencies[i] < frequencies[lessFrequent] ||
+            (frequencies[i] === frequencies[lessFrequent] && Math.random() < 0.5)
         )
             lessFrequent = i
 
-    const lessFrequentOrder = sessionOrders[lessFrequent] ? sessionOrders[lessFrequent] : []
+    const lessFrequentOrder = orders[lessFrequent] ? orders[lessFrequent] : []
     return lessFrequentOrder
 }
 
@@ -145,7 +136,7 @@ exports.getSessionOrder = functions.https.onRequest((request, response) =>
     respondErrors(request, response, async () => {
         if (enableCors(request, response, '*', 'POST')) return
         const orders = request.body
-        validateSessionOrder(orders)
-        response.status(200).send(await getLessFrequentSessionOrder(orders))
+        validateOrders(orders)
+        response.status(200).send(await getLessFrequentOrder(orders))
     })
 )
