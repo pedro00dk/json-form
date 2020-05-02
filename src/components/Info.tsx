0259af42b980329886186d5ca21472030024ce14
Pedro Henrique <pedro00dk@gmail.com>
2020-05-02 18:21:50 -0300

    Fix missing timer error in info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,6 +3,6 @@ import * as spec from '../spec'
 import { Session } from './Session'
 
 export const Info = (props: { info: string }) => {
-    const session: spec.Session = { questions: [{ content: [{ text: props.info }] }] }
+    const session: spec.Session = { timer: 0, questions: [{ content: [{ text: props.info }] }] }
     return <Session session={session} submit={{ enabled: false, text: '' }} onSubmit={() => {}} />
 }
